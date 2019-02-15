8fb4f34220ae32e85f85d4a7d8e564bf8db05f7a
Andrew J. Mauer <ajmauer@gmail.com>
2019-02-15 17:42:20 -0500

    updates to allow json file to be read in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,30 @@
 FROM ibmjava:8-jre
 #FROM maven:3.6.0-ibmjava-8
 
+RUN apt-get update
+RUN apt-get -y install vim
+RUN apt-get -y install curl
+RUN apt-get -y install zip
+
 COPY ./target/liberty/.  /
+COPY ./target/microprofile-health-1.0-SNAPSHOT.war /wlp/usr/servers/BoostServer/apps/
+RUN rm -rf /wlp/usr/servers/BoostServer/apps/*.xml
 
-RUN cd /
+EXPOSE 9080
 
-RUN ls -la
+# Set Path Shortcuts
+ENV PATH=/wlp/bin:/opt/ol/docker/:$PATH \
+    LOG_DIR=/logs \
+    WLP_OUTPUT_DIR=/wlp/output \
+    WLP_SKIP_MAXPERMSIZE=true
+
+#RUN mkdir /logs \
+#    && mkdir -p $WLP_OUTPUT_DIR/BoostServer \
+#    && ln -s $WLP_OUTPUT_DIR/BoostServer /output \
+#    && ln -s /wlp/usr/servers/BoostServer /config \
+#    && ln -s /logs $WLP_OUTPUT_DIR/BoostServer/logs \
+#    && ln -s /liberty /wlp
+
+#RUN ls -l /wlp
 
 CMD ["/wlp/bin/server", "run", "BoostServer"]
--- src/main/java/io/openliberty/guides/config/CustomConfigSource.java
+++ src/main/java/io/openliberty/guides/config/CustomConfigSource.java
@@ -22,12 +22,20 @@ import java.util.*;
 import java.io.StringReader;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
 public class CustomConfigSource implements ConfigSource {
 
-    String fileLocation = System.getProperty("user.dir").split("target")[0]
-            + "target/it/test-mpHealth-1.0/src/main/resources/CustomConfigSource.json";
+    //String fileLocation = System.getProperty("user.dir").split("target")[0]
+    //        + "src/main/resources/CustomConfigSource.json";
+	
+	//static InputStream in = getClass().getResourceAsStream("/CustomConfigSource.json");
+	
+	java.net.URL fileUrl = this.getClass().getResource("/CustomConfigSource.json");
+
+	String fileLocation = fileUrl.getFile();
 
     @Override
     public int getOrdinal() {
@@ -51,7 +59,7 @@ public class CustomConfigSource implements ConfigSource {
 
     public Map<String, String> getProperties() {
         Map<String, String> m = new HashMap<String, String>();
-        String jsonData = this.readFile(this.fileLocation);
+        String jsonData = this.readFile(fileLocation);
         JsonParser parser = Json.createParser(new StringReader(jsonData));
         String key = null;
         while (parser.hasNext()) {
@@ -83,9 +91,14 @@ public class CustomConfigSource implements ConfigSource {
     }
 
     public String readFile(String fileName) {
+				
+		System.out.println("AJM: got the json file now using an inputstream ...");
+		java.io.InputStream instr = getClass().getResourceAsStream("/CustomConfigSource.json");
+		
         String result = "";
         try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
+			BufferedReader br = new BufferedReader(new InputStreamReader(instr));
+            //BufferedReader br = new BufferedReader(new FileReader(fileName));
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
             while (line != null) {
